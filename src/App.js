f113254e1e2ed065eb9f6ab5bfcca8f6a3280c02
Graham <graham.hervey@gmail.com>
2018-03-13 14:09:02 -0400

    final front end design

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://aqueous-waters-61006.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -90,7 +90,7 @@ class App extends Component {
     .then(response =>
     {
       if(response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://aqueous-waters-61006.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
